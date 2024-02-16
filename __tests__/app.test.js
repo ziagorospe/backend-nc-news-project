0bdc5107f2b9c2fb97abb530fe9637a645a43de6
ziagorospe <ziagorospe@gmail.com>
2024-02-16 14:48:30 +0000

    added comment_count to get articles by id endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -59,22 +59,12 @@ describe('NC NEWS', () => {
             .expect(200)
         });
         test('should respond 200 and return article with specific ID when called', () => {
-            const validArticle = {
-                article_id: 1,
-                title: "Living in the shadow of a great man",
-                topic: "mitch",
-                author: "butter_bridge",
-                body: "I find this existence challenging",
-                created_at: "2020-07-09T20:11:00.000Z",
-                votes: 100,
-                article_img_url:
-                  "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
-              };
             return request(app)
             .get('/api/articles/1')
             .expect(200)
             .then((response)=>{
-                expect(response.body.article).toEqual(validArticle);
+                console.log(response.body)
+                expect(Object.keys(response.body.article).sort()).toEqual(['author','article_id','title','topic','created_at','votes','article_img_url','body','comment_count'].sort());
             })
         })
         test('should reject 400 when given an invalid article ID', () => {
